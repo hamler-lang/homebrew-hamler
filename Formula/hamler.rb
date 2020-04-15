260603dd8688752ad3a514b50b1b65472b21c561
zhanghongtong <zhanghongtong@foxmail.com>
2020-04-15 19:24:49 +0800

    Init

---
+++ Formula/hamler.rb
@@ -0,0 +1,39 @@
+class Hamler < Formula
+  homepage "https://emqx.io"
+  url "http://52.199.153.25/hamler/hamler-0.0.1.zip"
+  sha256 "7d09b51ec5db515adb4c72edbb1cd28dd14c11ac78d1121d71f4dd99d6e9d56e"
+  version "0.0.1"
+
+  depends_on "erlang"
+
+  def install
+    prefix.install Dir["*"]
+    bin.install Dir[libexec/"/bin/hamler"]
+  end
+
+  plist_options :manual => "hamler"
+
+  def plist; <<-EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{opt_bin}/hamler</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <!-- need erl in the path -->
+          <key>PATH</key>
+          <string>#{HOMEBREW_PREFIX}/sbin:/usr/bin:/bin:#{HOMEBREW_PREFIX}/bin</string>
+        </dict>
+      </dict>
+    </plist>
+    EOS
+  end
+
+end
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Hamler brew
+
+## How to install hamler
+
+```
+brew tap hamler/hamler
+brew install hamler/hamler
+```
