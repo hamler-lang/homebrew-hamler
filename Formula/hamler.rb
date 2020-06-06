5f7cd6e3043c735f71d8107e1a054b4ce5fdf33f
zhanghongtong <zhanghongtong@foxmail.com>
2020-06-06 16:38:26 +0800

    chore: change hamler version

--- Formula/hamler.rb
+++ Formula/hamler.rb
@@ -1,14 +1,13 @@
 class Hamler < Formula
-  homepage "https://emqx.io"
-  url "https://repos.emqx.io/hamler/hamler-0.0.2.tgz"
-  sha256 "fba0c76e2a6fb25846f8d53e7ae389e37e870b49cf2d563ed406c8e989df6494"
-  version "0.0.2"
+  homepage "https://hamler-lang.org/"
+  url "https://repos.emqx.io/hamler/hamler-0.1.tgz"
+  sha256 "ad0c791ffea995be61dca902372ffd1f1168d99dba21b4a420ff1cd72093a509"
+  version "0.1"
 
-  depends_on "erlang"
+  depends_on "erlang" => "22.1"
 
   def install
     prefix.install Dir["*"]
-    bin.install Dir[libexec/"/bin/hamler"]
   end
 
   plist_options :manual => "hamler"
@@ -37,7 +36,12 @@ class Hamler < Formula
   end
 
   def post_install
+    system "rm", "-rf", "/usr/local/lib/hamler"
     system "ln", "-s", "#{prefix}", "/usr/local/lib/hamler"
+  end
+
+  def post_uninstall
+    system "rm", "-rf", "/usr/local/lib/hamler"
   end
 
   test do
